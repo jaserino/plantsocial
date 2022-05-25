8348cbea63b168e8daffb60598c9ea183cd8425a
jaserino <jaserino1@gmail.com>
2022-05-25 13:14:29 -0400

    created checkout page and checkout cart functions

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,11 +1,12 @@
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
 
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+
 import "./checkout.styles.scss";
 
 const Checkout = () => {
-  const { cartItems, addItemToCart, removeItemFromCart } =
-    useContext(CartContext);
+  const { cartItems, cartTotal } = useContext(CartContext);
 
   return (
     <div className="checkout-container">
@@ -28,20 +29,10 @@ const Checkout = () => {
       </div>
       <h1>I am the Checkout page</h1>
 
-      {cartItems.map((cartItem) => {
-        const { imagePic, id, name, quantity } = cartItem;
-        return (
-          <div key={id}>
-            <h2>{name}</h2>
-            <span>{quantity}</span>
-            <br />
-            <span onClick={() => removeItemFromCart(cartItem)}>decrement</span>
-            <br />
-            <span onClick={() => addItemToCart(cartItem)}>increment</span>
-          </div>
-        );
-      })}
-      <span className="Total">Total: 0</span>
+      {cartItems.map((cartItem) => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+      <span className="total">Total: ${cartTotal} 0</span>
     </div>
   );
 };
